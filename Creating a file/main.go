4c3af6839ccbc490b3d67def09cf6ce6ad7bf686
Wesley Yan Soares Brehmer <102262759+NervousGroove@users.noreply.github.com>
2023-07-07 07:27:13 -0300

    Update main.go

--- Creating a file/main.go
+++ Creating a file/main.go
@@ -2,12 +2,21 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
 func main() {
-	nomes := []string{"Yan", "Wesley", "Brehmer"}
-	for _, nome := range nomes {
-		fmt.Println("saca só os nome tlg:", nome)
+	filename := "Your Filename.txt"
+	file, err := os.Create(filename)
+	if err != nil {
+		fmt.Println("Um erro ocorreu: ", err)
+	} else if err != nil {
+		fmt.Println("Nenhum erro, operação com sucesso")
 	}
+	content := "Your Content Here"
+	_, err = file.WriteString(content)
+	if err != nil {
 
+	}
+	defer file.Close()
 }
