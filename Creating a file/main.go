dc97ba0760b68ccba3f24ed6d72473c53df7f7ad
Wesley Yan Soares Brehmer <102262759+NervousGroove@users.noreply.github.com>
2023-07-07 07:26:08 -0300

    Add files via upload

---
+++ Creating a file/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	nomes := []string{"Yan", "Wesley", "Brehmer"}
+	for _, nome := range nomes {
+		fmt.Println("saca só os nome tlg:", nome)
+	}
+
+}
